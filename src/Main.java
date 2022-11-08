ce72637bdf8ff9f85bd6ebbbac55159f6f922efe
Sergey Petrenko <Stevo98@mail.ru>
2022-11-08 16:36:07 +1000

    second commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 30_000;
+        byte mile = 20;
+        int amount = ticket/mile;
+        System.out.println("Ваши бонусные мили - " + amount);
+    }
+}
